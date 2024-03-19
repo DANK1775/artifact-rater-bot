e84df99bfc08cc649e1f1f16df2490825c298047
DANK??? <96366744+DANK1175@users.noreply.github.com>
2024-03-19 16:10:37 -0600

    Add files via upload

--- commands/functions/emojis.js
+++ commands/functions/emojis.js
@@ -1,3 +1,12 @@
 const emoji = {
-    
+    BDV: '<:derechoVacio:1219676507664814191>',  // borde derecho vacio
+    BDL: '<:derechoLleno:1219676540493889536>', // borde derecho lleno
+    BV: '<:vacio:1219676427931353199>', // barra vacia
+    BL: '<:lleno:1219676379914829905>', // Barra llena
+    BIV: '<:izquierdoVacio:1219731525679972432>',  // borde izquierzo vacio
+    BIL: '<:izquierdoLleno:1219676329709011004>', // borde izquierzo lleno
+}
+
+module.exports = {
+    emoji
 }
--- commands/functions/linea.js
+++ commands/functions/linea.js
@@ -1,19 +1,42 @@
-let symbol = '▃▃'
+const { emoji } = require('./emojis')
+
+let emojiBar = ''
+let emojiCount = 1
 let n = 0
+
+function nbar(n) {
+    emojiCount = 1
+    emojiBar = ''
+
+    for (let i = 1; i < n; i++) {
+        emojiBar += emoji.BL
+        emojiCount++
+    }
+
+    for (let x = 0; x < (9 - emojiCount); x++) {
+        emojiBar += emoji.BV
+    }
+
+    return emojiBar
+
+}
 function linea(cantidad) {
+    n = 0
     if (isNaN(cantidad)) {
         n = parseFloat(cantidad.replace('%', ''))
     } else {
         n = cantidad
     }
-    
+
     let redondear = Math.round(n)
-    n = redondear >= 100? parseInt(redondear.toString()[0] + 0): parseInt(redondear.toString()[0])
-    if (n === 0) {
-    return '▃'
-    } else {
-        return lineaConSymbol = symbol.repeat(n)
-    }
+
+    n = redondear >= 100 ? parseInt(redondear.toString()[0] + 0) : parseInt(redondear.toString()[0])
+    if (n === 0) return `${emoji.BIV}${nbar(n)}${emoji.BDV}`
+
+    if ((n > 1) && (n < 10)) return `${emoji.BIL}${nbar(n)}${emoji.BDV}`
+
+    if (n === 10) return `${emoji.BIL}${nbar(n)}${emoji.BDL}`
+
 }
 
 module.exports = {
