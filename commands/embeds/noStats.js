ebf20398dfb88d5653a39340883ec1722304985e
DANK??? <96366744+DANK1175@users.noreply.github.com>
2024-03-19 16:08:13 -0600

    Add files via upload

--- commands/embeds/noStats.js
+++ commands/embeds/noStats.js
@@ -4,7 +4,7 @@ const { randomIcon } = require('../functions/randomIcon')
 const notFoundStats = () => {
      return new EmbedBuilder()
     .setTitle("**Error stats not found**")
-    .setDescription("**No se indetifico ninguna stat**\n\n* **Debes proporcionar lo siguiente:** \n * El idioma del juego debe estar en espalñol \n * Presenta las stats en la vista de info de artefacto no de mejora \n * Imagen clara de las stats")
+    .setDescription("**No se indetifico ninguna stat**\n\n* **Debes proporcionar lo siguiente:** \n * El idioma del juego debe estar en español \n * Presenta las stats en la vista de info de artefacto no de mejora \n * Imagen clara de las stats")
     .setThumbnail(randomIcon())
     .setColor("#ff0000")
     .setTimestamp();
